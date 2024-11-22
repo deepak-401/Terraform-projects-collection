f004bfd9f53e42d029c987b26c8023afb2c64668
Deepak Kumar Patra <145733842+deepak-401@users.noreply.github.com>
2024-11-22 10:38:17 +0530

    Delete providers.tf

--- providers.tf
+++
@@ -1,6 +0,0 @@
-# Define the AWS provider configuration.
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIA3FLD4NZBPXZVOJPADEE"
-  secret_key = "DrbmZB3RC9TqEZUWB5oTQjcXgDaV5p9rVDPM+BeLZEe"
-}
