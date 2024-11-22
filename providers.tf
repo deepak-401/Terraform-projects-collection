8fa108cb6a89e965c8316b55c0816e7791e87f75
Deepak Kumar Patra <145733842+deepak-401@users.noreply.github.com>
2024-11-22 10:24:49 +0530

    providers.tf file added

---
+++ providers.tf
@@ -0,0 +1,6 @@
+# Define the AWS provider configuration.
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIA3FLD4NZBPXZVOJPADEE"
+  secret_key = "DrbmZB3RC9TqEZUWB5oTQjcXgDaV5p9rVDPM+BeLZEe"
+}
